05605f634b5be93d95f7e6be87e06bb863542cca
Tello, Juan Pablo <jtello@huenei.com>
2019-10-29 14:57:48 -0300

    Change content to server.js 4

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/TiendaDanyQx'));
+app.use(express.static('./dist/TiendaDanyQx'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join('/dist/TiendaDanyQx/index.html'));
+    res.sendFile(path.join('./dist/TiendaDanyQx/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
