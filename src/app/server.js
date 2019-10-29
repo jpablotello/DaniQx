e759f74b8253b2717b1cca42c8d561339d756a02
Tello, Juan Pablo <jtello@huenei.com>
2019-10-29 14:16:30 -0300

    Add server.js to app directory

--- src/app/server.js
+++ src/app/server.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const path = require('path');
-
+import express from "express";
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+app.use(static(__dirname + '/dist'));
+
+app.all('*', (req, res) => {
+    res.status(200).sendFile(__dirname + '/dist/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
